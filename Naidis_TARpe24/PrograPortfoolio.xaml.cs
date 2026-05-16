e2bdbef06ba9689fe709e6adc2d6d7536e804792
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-05-16 11:33:04 +0300

    Progra portfoolio
    Added hello world in each language

--- Naidis_TARpe24/PrograPortfoolio.xaml.cs
+++ Naidis_TARpe24/PrograPortfoolio.xaml.cs
@@ -4,6 +4,7 @@ public class CarouselProgra
 {
 	public string Title { get; set; }
 	public string ImageUrl { get; set; }
+	public string TextBoxHelloWorld { get; set; }
 }
 public partial class PrograPortfoolio : ContentPage
 {
@@ -16,11 +17,11 @@ public partial class PrograPortfoolio : ContentPage
 
 		var items = new List<CarouselProgra>
 		{
-			new CarouselProgra { Title = "Rust", ImageUrl = "https://rust-on-nails.com/blog/rust-complicated/rust.jpg"},
-			new CarouselProgra { Title = "Assembly", ImageUrl = "https://media.licdn.com/dms/image/v2/D4D12AQEM9F_-u1OT5Q/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1658763190886?e=2147483647&v=beta&t=aEp72fzTRZC_0EilFbT0wVV2jw6O-Bqt759qLG4pLrw"},
-            new CarouselProgra { Title = "C++", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1280px-ISO_C%2B%2B_Logo.svg.png"},
-            new CarouselProgra { Title = "C", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/960px-C_Programming_Language.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail"},
-            new CarouselProgra { Title = "HolyC", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/HolyC_Logo.svg/1920px-HolyC_Logo.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail"},
+			new CarouselProgra { Title = "Rust", ImageUrl = "https://rust-on-nails.com/blog/rust-complicated/rust.jpg", TextBoxHelloWorld = "\"\"\r\nfn main() {\r\n println!(\"Hello, World!\");\r\n}\r\n\"\""},
+			new CarouselProgra { Title = "Assembly", ImageUrl = "https://media.licdn.com/dms/image/v2/D4D12AQEM9F_-u1OT5Q/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1658763190886?e=2147483647&v=beta&t=aEp72fzTRZC_0EilFbT0wVV2jw6O-Bqt759qLG4pLrw", TextBoxHelloWorld = "\"\"\r\n section .data\r\n msg db \"Hello, World!\", 10\r\n msg_len equ $ - msg\r\n\r\n section .text\r\n global _start\r\n\r\n _start:\r\n mov rax, 1\r\n mov rdi, 1\r\n mov rsi, msg\r\n mov rdx, msg_len\r\n syscall\r\n\r\n mov rax, 60\r\n mov rdi, 0\r\n syscall\r\n  \"\""},
+            new CarouselProgra { Title = "C++", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1280px-ISO_C%2B%2B_Logo.svg.png", TextBoxHelloWorld = "\"\"\r\n#include <iostream>\r\n\r\n int main() {\r\n std::cout << \"Hello, World!\" << std::endl;\r\n return 0;\r\n }\r\n \"\""},
+            new CarouselProgra { Title = "C", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/960px-C_Programming_Language.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail", TextBoxHelloWorld = "\"\"\r\n #include <stdio.h>\r\n\r\n int main() {\r\n printf(\"Hello, World!\\n\");\r\n return 0;\r\n }\r\n \"\""},
+            new CarouselProgra { Title = "HolyC", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/HolyC_Logo.svg/1920px-HolyC_Logo.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail", TextBoxHelloWorld = "\"\"\r\n U0 HelloWorld()\r\n {\r\n \"Hello, World!\\n\";\r\n }\r\n HelloWorld;\r\n \"\""},
         };
 		var carouselProgra = new CarouselView
 		{
@@ -72,6 +73,14 @@ public partial class PrograPortfoolio : ContentPage
 				HorizontalOptions = LayoutOptions.Start
 			};
 			label.SetBinding(Label.TextProperty, "Title");
+
+			var tap = new TapGestureRecognizer();
+			tap.Tapped += async (s, e) =>
+			{
+				var tappedItem = ((Frame)s).BindingContext as CarouselProgra;
+				await DisplayAlertAsync("Kuidas kirjutada tere maailm?", tappedItem?.TextBoxHelloWorld ?? "Pole olemas", "OK");
+			};
+			frame.GestureRecognizers.Add(tap);
 
 			grid.Children.Add(image);
 			grid.Children.Add(gradient);
